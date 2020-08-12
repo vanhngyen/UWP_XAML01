1b6b40b943e4f9346d2388bc4d8e435060439414
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2020-08-12 23:19:49 +0700

    b2 - binding data

--- UWP_XAML01/App.xaml.cs
+++ UWP_XAML01/App.xaml.cs
@@ -66,7 +66,7 @@ namespace UWP_XAML01
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(WrapGrid), e.Arguments); //sua ở đây
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments); //sua ở đây
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
--- UWP_XAML01/RelativePanel.xaml.cs
+++ UWP_XAML01/RelativePanel.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using UWP_XAML01.models;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -14,27 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace UWP_XAML01
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class RelativePanel : Page
     {
-        private List<Book> Books;
-        public MainPage()
+        public RelativePanel()
         {
             this.InitializeComponent();
-            Books = BookManager.GetBooks();
-        }
-
-        private void GridView_ItemClick(object sender, ItemClickEventArgs e)
-        {
-            var book = (Book)e.ClickedItem;
-            ResultTextBlock.Text = "You selected " + book.Title;
         }
     }
 }

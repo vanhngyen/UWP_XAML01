b5d8b75aa3ec88d90bb6675c9f432a2e921b8968
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2020-08-19 22:17:10 +0700

    weatherAppDemo

---
+++ WeatherAppDemo/LocationData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace WeatherAppDemo
+{
+    class LocationData
+    {
+        public async static Task<Geoposition> getPosition()
+        {
+            var accessStatus = await Geolocator.RequestAccessAsync();
+            if (accessStatus != GeolocationAccessStatus.Allowed) throw new Exception();
+            var geolocaltor = new Geolocator { DesiredAccuracyInMeters = 0 };
+            var postion = await geolocaltor.GetGeopositionAsync();
+            return postion;
+        }
+       
+    }
+}

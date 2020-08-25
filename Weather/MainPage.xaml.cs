e59d33efac3ba60af8c2a84953a16e405aa547f6
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2020-08-25 20:18:41 +0700

    push

---
+++ Weather/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
+using Weather.Models;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Weather
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        ObservableCollection<WeatherJSON> WeatherEachHours;
+        ObservableCollection<DailyForecast> WeatherEachDays;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            WeatherEachHours = new ObservableCollection<WeatherJSON>();
+            InitJSon();
+
+            WeatherEachDays = new ObservableCollection<DailyForecast>();
+            InitEachDaysJSON();
+        }
+
+        private async void InitJSON()
+        {
+            var url = "http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/353412"+
+                "apikey=tbFOLXfZmAxAexEYOmXhcxnbZBDjQBSh&language=vi-vn&metric=true";
+
+            var list = await WeatherJSON.GetJSON(url) as List<WeatherJSON>;
+            Debug.WriteLine("Count : " + list.Count);
+
+            list.ForEach(it =>
+            {
+                var match = Regex.Matches(it.DateTime, "T(?<time>\\d+):")[0].Groups["time"].Value;
+                if (int.Parse(match) > 12)
+                {
+                    match = (int.Parse(match) - 12) + "PM";
+                }
+                else
+                {
+                    match += "PM";
+                }
+                it.DateTime = match;
+                it.Temperature.Value += "\u00B0";
+                it.WeatherIcon = string.Format("https://vortex.accuweather.com/adc2010/images/slate/icons/{0}.svg",it.WeatherIcon);
+                WeatherEachHours.Add(it);
+            });
+           
+            WeatherDescriptionTextBlock.Text = list[0].IconPhrase;
+            WeatherTemperatureTextBlock.Text = list[0].Temperature.Value;
+        }
+        private async void InitEachDaysJSON()
+        {
+            var urlFiveDay = ""
+        }
+    }
+}

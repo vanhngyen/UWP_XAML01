e85d0b2f836fab36f9ffe4628125094062d2e4a2
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2020-08-26 22:03:21 +0700

    UWPSQLiteDemo1

--- UWPSQLiteDemo1/MainPage.xaml.cs
+++ UWPSQLiteDemo1/MainPage.xaml.cs
@@ -1,17 +1,28 @@
-﻿using System.IO;
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.ConversationalAgent;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using SQLite;
-
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
-namespace UWPSQLiteDemo
+namespace UWPSQLiteDemo1
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        
         string path;
         SQLite.Net.SQLiteConnection conn;
         public MainPage()
@@ -27,7 +38,7 @@ namespace UWPSQLiteDemo
             string id = "";
             string name = "";
             string age = "";
-            foreach (var message in query)
+            foreach(var message in query)
             {
                 id = id + " " + message.Id;
                 name = name + " " + message.Name;
@@ -46,7 +57,7 @@ namespace UWPSQLiteDemo
     }
     public class Customer
     {
-        [PrimaryKey, AutoIncrement]
+        [PrimaryKey,AutoIncrement]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Age { get; set; }

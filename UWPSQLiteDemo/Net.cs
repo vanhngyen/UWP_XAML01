655e26fe54323986e9750dd1cfc36b36b9ff5a30
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2020-08-26 19:00:03 +0700

    UWPSQLiteDemo

---
+++ UWPSQLiteDemo/Net.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace UWPSQLiteDemo
+{
+    internal class Net
+    {
+        internal class SQLiteConnection
+        {
+            public SQLiteConnection(Net.Platform.WinRT.SQLitePlatformWinRT sQLitePlatformWinRT, string path)
+            {
+            }
+
+            internal object Insert(Customer customer)
+            {
+                throw new NotImplementedException();
+            }
+
+            internal object Table<T>()
+            {
+                throw new NotImplementedException();
+            }
+
+            internal void CreateTable<T>()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        internal class Platform
+        {
+            internal class WinRT
+            {
+                internal class SQLitePlatformWinRT
+                {
+                    public SQLitePlatformWinRT()
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
--- Weather/MainPage.xaml.cs
+++ Weather/MainPage.xaml.cs
@@ -34,19 +34,15 @@ namespace Weather
             this.InitializeComponent();
             WeatherEachHours = new ObservableCollection<WeatherJSON>();
             InitJSON();
-
             WeatherEachDays = new ObservableCollection<DailyForecast>();
             InitEachDaysJSON();
         }
 
         private async void InitJSON()
         {
-            var url = "http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/353412"+
-                "apikey=tbFOLXfZmAxAexEYOmXhcxnbZBDjQBSh&language=vi-vn&metric=true";
-
+            var url = "http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/353412?apikey=93Qg780lHwYM4SO58n7DFPLqHg4oKADn&language=vi-vn&metric=true";
             var list = await WeatherJSON.GetJSON(url) as List<WeatherJSON>;
-            Debug.WriteLine("Count : " + list.Count);
-
+            Debug.WriteLine("Count:" + list.Count);
             list.ForEach(it =>
             {
                 var match = Regex.Matches(it.DateTime, "T(?<time>\\d+):")[0].Groups["time"].Value;
@@ -58,18 +54,22 @@ namespace Weather
                 {
                     match += "PM";
                 }
+
                 it.DateTime = match;
                 it.Temperature.Value += "\u00B0";
-                it.WeatherIcon = string.Format("https://vortex.accuweather.com/adc2010/images/slate/icons/{0}.svg",it.WeatherIcon);
+                it.WeatherIcon = string.Format("https://vortex.accuweather.com/adc2010/images/slate/icons/{0}.svg",
+                it.WeatherIcon);
                 WeatherEachHours.Add(it);
+
             });
-           
             WeatherDescriptionTextBlock.Text = list[0].IconPhrase;
             WeatherTemperatureTextBlock.Text = list[0].Temperature.Value;
         }
+
         private async void InitEachDaysJSON()
         {
             var urlFiveDay = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/353412?apikey=93Qg780lHwYM4SO58n7DFPLqHg4oKADn&language=vi-vn&metric=true";
+            /* var obj = await WeatherEachDay.GetWeatherEach(urlFiveDay) as WeatherEachDay;*//**/
             var obj = await WeatherEachDay.GetWeatherEach(urlFiveDay) as WeatherEachDay;
             obj.DailyForecasts.ForEach(it =>
             {
@@ -77,12 +77,12 @@ namespace Weather
                 var date = new DateTime(int.Parse(matchs[0].Value), int.Parse(matchs[1].Value), int.Parse(matchs[2].Value));
                 it.Date = date.DayOfWeek.ToString();
                 it.Day.Icon = string.Format("https://vortex.accuweather.com/adc2010/images/slate/icon/{0}.svg",
-                    it.Day.Icon);
+                it.Day.Icon);
                 Debug.WriteLine("Binh : " + it.Date);
                 WeatherEachDays.Add(it);
 
             });
-            Today.Text = WeatherEachDays[0].Date + "  Today";
+            Today.Text = WeatherEachDays[0].Date + " Today";
             MaxTemperature.Text = WeatherEachDays[0].Temperature.Maximum.Value + "";
             MinTemperature.Text = WeatherEachDays[0].Temperature.Minimum.Value + "";
             WeatherEachDays.RemoveAt(0);
